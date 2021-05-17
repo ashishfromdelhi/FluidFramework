3f467c7158ede5387779e2f0665366abbde803df
Vikram Agrawal <vikagrawal@microsoft.com>
2021-05-17 20:17:40 +0530

    Simplify runner script.

--- packages/test/test-service-load/runloadtest.ps1
+++ packages/test/test-service-load/runloadtest.ps1
@@ -7,7 +7,9 @@ function RunLoadTest {
 		[Parameter(Mandatory = $false)]
         [string]$Profile = 'BaseScenario_2.5',
 		[Parameter(Mandatory = $false)]
-        [string]$Namespace = 'odsp-perf-lg-fluid'
+        [string]$Namespace = 'odsp-perf-lg-fluid',
+        [Parameter(Mandatory = $false)]
+        [string]$TestUid = [guid]::NewGuid()
     )
 
     if ( ( $NumOfDocs -gt 10 ) -and ( $Namespace -ne 'odsp-perf-lg-fluid' ) ) {
@@ -15,12 +17,14 @@ function RunLoadTest {
         return
     }
 
+    Write-Output "Running TestUid: $TestUid"
+
     $Profiles = Get-Content -Raw -Path .\testConfig.json | ConvertFrom-Json
     [int]$NumOfUsersPerDoc = $Profiles.profiles.$Profile.numClients
     Write-Host "Starting RunLoadTest NumOfDocs: $NumOfDocs, Profile: $Profile, NumOfUsersPerDoc: $NumOfUsersPerDoc"
 	kubectl config set-context --current --namespace=$Namespace | out-null
 	CreateInfra -NumOfPods $NumOfDocs -Namespace $Namespace
-	RunTest -Profile $Profile -Namespace $Namespace -NumOfUsersPerDoc $NumOfUsersPerDoc -NumOfDocs $NumOfDocs
+	RunTest -Profile $Profile -Namespace $Namespace -NumOfUsersPerDoc $NumOfUsersPerDoc -NumOfDocs $NumOfDocs -TestUid $TestUid
 }
 
 function CreateInfra{
@@ -36,16 +40,16 @@ function CreateInfra{
 	kubectl create namespace $Namespace
 	kubectl apply -f load-generator-fluid-app.yaml -n $Namespace
     kubectl scale deployments lg-fluidapp -n $Namespace --replicas=$NumOfPods
-    $RunningNumOfPods = $(kubectl get pods -n $Namespace --field-selector status.phase=Running).count -1
+    $RunningNumOfPods = $(kubectl get pods -n $Namespace --field-selector status.phase=Running).count - 1
     while ($NumOfPods -ne $RunningNumOfPods) {
         Write-Host "Pods are in-progress"
         Start-Sleep -s 10
-        $RunningNumOfPods = $(kubectl get pods -n $Namespace  --field-selector status.phase=Running).count -1
+        $RunningNumOfPods = $(kubectl get pods -n $Namespace  --field-selector status.phase=Running).count - 1
     }
     Write-Host "Pods are created and running"
 }
 
-workflow RunTest{
+function RunTest{
 	[CmdletBinding()]
     Param(
 		[Parameter(Mandatory = $true)]
@@ -55,63 +59,80 @@ workflow RunTest{
         [Parameter(Mandatory = $true)]
         [int]$NumOfUsersPerDoc,
         [Parameter(Mandatory = $true)]
-        [int]$NumOfDocs
+        [int]$NumOfDocs,
+        [Parameter(Mandatory = $true)]
+        [string]$TestUid
     )
 
     $Tenants = (Get-Content -Raw -Path testTenantConfig.json | ConvertFrom-Json).tenants
-    $TenantsCount = ($Tenants | Get-Member -MemberType NoteProperty).Count
+    $TenantNames = $Tenants | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name
+    $TenantsCount = $TenantNames.Count
 	$Pods = $(kubectl get pods -n $Namespace --field-selector status.phase=Running -o json | ConvertFrom-Json).items
-    $TestUid = [guid]::NewGuid()
     [int]$PodsCount = $Pods.count
 
     if ( $PodsCount -ne $NumOfDocs ) {
         Write-Error "Number of pods not equal to number of docs"
         return
-    }
+     }
 
     Write-Output "Load Starting ${testUid}"
 
-    $Started = 0
+    $Configs = @{}
+    $TempPath = $env:TEMP
+    $Index = 0
+    foreach ($Tenant in $TenantNames) {
+        $ConfigFileName = (Join-Path -Path $TempPath -ChildPath ($Tenant + ".json"))
+        $TenantContent = $Tenants.$Tenant
+        $TenantContent | ConvertTo-Json | Out-File -Encoding ascii -FilePath $ConfigFileName
+        $Configs.Add($Index, $ConfigFileName)
+        $Index++
+    }
 
-    foreach -parallel -ThrottleLimit 10 ($i in 1..$PodsCount) {
-        $SleepTime = Get-Random -Minimum 5 -Maximum 30
+    foreach ($i in 1..$PodsCount) {
+        $SleepTime = Get-Random -Minimum 0 -Maximum 3
         sleep $SleepTime
 
         $PodName = $Pods[$i - 1].metadata.name
         $TenantIndex = ($i-1) % $TenantsCount
-        $TenantName = ($Tenants | Get-Member -MemberType NoteProperty)[$TenantIndex].Name
-        $ConfigFileName = $PodName + ".json"
-        $TenantContent = $Tenants.$TenantName
         $RemoteFile = "$Namespace/${PodName}:/app/packages/test/test-service-load/testUserConfig.json"
 
-        InlineScript {
-            cd $env:TEMP
-            $using:TenantContent | ConvertTo-Json | Out-File -Encoding ascii -FilePath $using:ConfigFileName
-            kubectl cp $using:ConfigFileName $using:RemoteFile
-        }
-
+        Write-Output "Copying config file to $PodName"
+        kubectl cp $Configs[$TenantIndex] $RemoteFile
 
         $Command = "FLUID_TEST_UID='$TestUid' node ./dist/nodeStressTestMultiUser.js -p $Profile > testscenario.logs 2>&1 &"
+
         Write-Output "Exec Command: $Command on Pod: $PodName"
-
         kubectl exec $PodName -n $Namespace -- bash -c $Command
-
-        kubectl exec $PodName -n $Namespace -- bash -c "ps -aux | grep node | grep -v grep"
-        if ($LastExitCode) {
-            Write-Error "Error in starting process on pod ${PodName}. Trying once more."
-
-            kubectl exec $PodName -n $Namespace -- bash -c $Command
-            kubectl exec $PodName -n $Namespace -- bash -c "ps -aux | grep node | grep -v grep"
-            if ($LastExitCode) {
-                Write-Error "Error in starting process on pod ${PodName}. Retry failed. Exiting"
-                Exit 1
-            }
-        }
-
-        $workflow:Started++
-
         Write-Output "Exec Command DONE: on Pod: $PodName"
+
+        Write-Output "Completed $i"
+        Write-Output ""
     }
 
 	Write-Output "Load Submitted. Started ${Started} pods. TestUid: ${TestUid}"
+}
+
+function CheckTest{
+	[CmdletBinding()]
+    Param(
+		[Parameter(Mandatory = $true)]
+        [string]$Namespace
+    )
+
+	$Pods = $(kubectl get pods -n $Namespace --field-selector status.phase=Running -o json | ConvertFrom-Json).items
+    [int]$PodsCount = $Pods.count
+
+    Write-Output "Checking test"
+
+    foreach ($i in 1..$PodsCount) {
+        $PodName = $Pods[$i - 1].metadata.name
+        $Command = "ps -a | grep node"
+
+        Write-Output "$PodName starting"
+        kubectl exec $PodName -n $Namespace -- bash -c $Command
+        if ($? -eq $false) {
+            Exit 1
+        }
+        Write-Output "$PodName starting"
+    }
 }
